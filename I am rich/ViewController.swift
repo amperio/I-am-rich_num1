de4b843154998e68f31e0d2397a468b95dcd7cfb
Juan Daniel <pericles_navarro@hotmail.com>
2021-05-03 09:32:42 -0500

    Add files via upload

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Juan Navarro  on 11/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
